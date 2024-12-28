db71c67ac7f88559ba4d1bccecbcc4b30575ce4c
Domenico <domfalco2001@gmail.com>
2024-12-28 17:28:57 +0100

    Modifiche apportate: - modificato il file main.py, è stato svolto il lavoro per la manipolazione dei dati presenti nel dataset; - aggiunto il file DecisionTree.py; - aggiunto il file LogisticRegression.py; - aggiunto il file RandomForest.py; - aggiunto il dataset diabetes.csv dove verrà svolto il lavoro per il progetto.

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import kagglehub
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def remove_outliers(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+def main():
+    ds = pd.read_csv('diabetes.csv') #lettura dataset
+    """#stampa l'intero dataset
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    print(ds) 
+    ds.info()  # mostra info sul dataset
+    print(ds.isnull().any()) # ricerca di valori nulli
+    print(ds.duplicated().sum()) #ricerca di eventuali duplicati """
+
+    #creazione dei grafici per le varie colonne per la distribuzione dei valori
+    fig, axes = plt.subplots(3, 3, figsize=(15, 10))
+    for i, col in enumerate(ds.columns):
+        row, col_position = divmod(i, 3)
+        sns.boxplot(data=ds, y=col, ax=axes[row, col_position])
+        plt.tight_layout()
+    plt.show()
+
+    for col in ds.columns:
+        ds = remove_outliers(ds, col)
+    
+    
+if __name__ == "__main__":
+    main()
